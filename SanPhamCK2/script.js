451f74f61ba5ed5b7e1ba9164ba0e795a0b40167
zxasqw1234 <32673703+zxasqw1234@users.noreply.github.com>
2024-08-12 23:45:59 +0700

    Add files via upload

---
+++ SanPhamCK2/script.js
@@ -0,0 +1,61 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('dropdownButton');
+    const menu = document.getElementById('dropdownMenu');
+
+    button.addEventListener('click', () => {
+        const expanded = button.getAttribute('aria-expanded') === 'true' || false;
+        button.setAttribute('aria-expanded', !expanded);
+        menu.style.display = expanded ? 'none' : 'block';
+    });
+
+    // Close the menu when clicking outside of it
+    document.addEventListener('click', (event) => {
+        if (!button.contains(event.target) && !menu.contains(event.target)) {
+            button.setAttribute('aria-expanded', 'false');
+            menu.style.display = 'none';
+        }
+    });
+
+    // Keyboard interaction
+    button.addEventListener('keydown', (event) => {
+        if (event.key === 'Escape') {
+            button.setAttribute('aria-expanded', 'false');
+            menu.style.display = 'none';
+        }
+    });
+
+    menu.addEventListener('keydown', (event) => {
+        if (event.key === 'Escape') {
+            button.setAttribute('aria-expanded', 'false');
+            menu.style.display = 'none';
+        }
+    });
+});
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+      watchState: true,
+    },
+  });
+  let mybutton = document.getElementById("myBtn");
+
+  window.onscroll = function() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          mybutton.style.display = "block";
+      } else {
+          mybutton.style.display = "none";
+      }
+  };
+
+  function scrollToTop() {
+      document.body.scrollTop = 0; // For Safari
+      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+  }
